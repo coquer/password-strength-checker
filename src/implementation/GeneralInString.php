02676c1a5867fb065f922e8812c39d18060e1a2c
Jorge Y. C. Rodriguez <developer@jorgerodriguez.dk>
2016-04-23 18:43:43 +0200

    more method to get score

--- src/implementation/GeneralInString.php
+++ src/implementation/GeneralInString.php
@@ -4,23 +4,44 @@ namespace jycr753\PasswordStrengthChecker;
 
 class GeneralInString extends GeneralInStringAbstract{
 
-    public function hasCharactersOnly()
+    public function hasCharactersOnly( array $password )
     {
-        // TODO: Implement hasCharactersOnly() method.
+        $passwordLength = strlen( $password[ 'raw' ] );
+
+        return (
+            $password[ 'lower' ][ 'count' ] + $password[ 'upper' ][ 'count' ] == $passwordLength
+        ) ? - $passwordLength : 0;
     }
 
-    public function hasExtra()
+    public function hasExtra( array $password )
     {
-        // TODO: Implement hasExtra() method.
+        $score = 0;
+        if ( strlen( $password[ 'raw' ] ) >= 8 ) {
+            $score += 2;
+        }
+        foreach ( [ 'upper', 'lower', 'number', 'symbol' ] as $type ) {
+            if ( $password[ $type ][ 'count' ] > 0 ) {
+                $score += 2;
+            }
+        }
+
+        return $score;
     }
 
-    public function hasNumbersOnly()
+    public function hasNumbersOnly( array $password )
     {
-        // TODO: Implement hasNumbersOnly() method.
+        $passwordLength = strlen( $password[ 'raw' ] );
+
+        return (
+            $password[ 'number' ][ 'count' ] == $passwordLength
+        ) ? - $passwordLength : 0;
     }
 
-    public function hasAnyNumberInTheMiddle()
+    public function hasAnyNumberInTheMiddle( array $password )
     {
-        // TODO: Implement hasAnyNumberInTheMiddle() method.
+        $password = substr( $password[ 'raw' ], 1, strlen( $password[ 'raw' ] ) - 2 );
+        preg_match_all( '/[0-9]{1}/', $password, $matches );
+
+        return ( isset( $matches[ 0 ] ) ) ? count( $matches[ 0 ] ) * 2 : 0;
     }
 }
